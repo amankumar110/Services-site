311894cc4d9cb1030fc8d580d9375906f1d410a1
amankumar110 <115738540+amankumar110@users.noreply.github.com>
2023-03-15 23:28:14 +0500

    version 3 complete

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
 const form = document.querySelector(".information-form");
 const alertbox = document.querySelector(".alertdiv");
 const alerttoggle = document.querySelector(".alert-toggle");
-const alertwarning = document.querySelector(".alertwarning")
+const alertwarning = document.querySelector(".alertwarning");
+const herodiv = document.querySelector(".hero");
+const herotext = document.querySelector(".herotext");
+const imgsources = ["background-image:url(1.jpg);","background-image:url(2.jpg);", "background-image:url(3.jpg);","background-image:url(4.jpg);"]
+const herobrandings = ["design for yourself","Prove Yourself","Show them","It is a right Path"];
+const dots = document.querySelectorAll(".dots");
+let index = 0;
+let imgindex = 0 ;
+setInterval(() => {
+    index= (index+1) % herobrandings.length;
+    imgindex = (imgindex+1) % imgsources.length;
+    herodiv.setAttribute("style",imgsources[imgindex]);
+    herotext.textContent = herobrandings[index];
+    dots[index].style.color ="white";
+    setTimeout(() => {
+      dots[index].style.color = "darkgrey"
+    }, 2960);
 
-// if (client.value ==="" || email.value ==="" || description.value ==="") {
-//     alert("something is not filled")
-// }
+  }, 3000);
 
-form.addEventListener("submit",(e)=>{
-        e.preventDefault();
-        const client = form.clientname;
-const email = form.clientemail;
-const description = form.clientdescription;
-        function check(input){
-            alerttoggle.addEventListener("click",()=>{
-                alertbox.classList.remove("d-flex");
-                input.style.outline = "none";
-             });
-             
-            if (input.value ==="") {
-                alertbox.classList.add("d-flex");
-                alertwarning.textContent = "Form is unfilled";
-                input.style.outline ="1px solid red";
-            } 
-        }
-        check(client);
-        check(email);
-        check(description);
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const client = form.clientname;
+  const email = form.clientemail;
+  const description = form.clientdescription;
+  function check(input) {
+    alerttoggle.addEventListener("click", () => {
+      alertbox.classList.remove("d-flex");
+      input.style.outline = "none";
+    });
+    if (input.value === "") {
+      alertbox.classList.add("d-flex");
+      alertwarning.textContent = "Form is unfilled";
+      input.style.outline = "1px solid red";
+    }
+  }
+  check(client);
+  check(email);
+  check(description);
+});
